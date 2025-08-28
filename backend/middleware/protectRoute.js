ec2eff69a767c806c226e0f3b74b816f4eaf06b5
Loredounipass <108556798+Erick-MC-Cedeno@users.noreply.github.com>
2025-08-28 00:32:52 +0000

    New Updates now

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user-schema/user.schema.js";
 
+
 const protectRoute = async (req, res, next) => {
 	try {
 		const token = req.cookies.jwt;
